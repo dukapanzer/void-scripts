ee78e7e690e78d98b04c58957d10863ec4a11ff5
skidded <131218588+dukapanzer@users.noreply.github.com>
2024-08-04 00:35:41 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -3,9 +3,6 @@ local char = player.Character
 
 local humanoid = char:WaitForChild("Humanoid")
 
-local Assets = LoadAssets(18787489967)
-Assets.Parent = workspace
-
 local function play_anim(animation)
   local animations = workspace:WaitForChild("Animations")
 
