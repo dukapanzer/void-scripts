e8aaa6c52b2553c502844a6f916f55af9cb56148
skidded <131218588+dukapanzer@users.noreply.github.com>
2024-08-04 01:09:16 +0300

    Update and rename assd.lua to sfdg.lua

--- sfdg.lua
+++ sfdg.lua
@@ -3,11 +3,10 @@ local char = player.Character
 
 local humanoid = char:WaitForChild("Humanoid")
 
-local function play_anim()
   local anim = workspace.Animations["Neptunian_V"]
+print(anim)
 
   local load_animation = humanoid:LoadAnimation(anim)
   load_animation:Play()
-end
 
 play_anim()
