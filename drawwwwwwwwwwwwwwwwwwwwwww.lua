8c261f64c28f30f96ea508b63c89e01764a0019e
skidded <131218588+dukapanzer@users.noreply.github.com>
2024-08-23 11:57:12 +0300

    Update and rename drawwwwwwwwwwwwwwwwwwwwww.lua to drawwwwwwwwwwwwwwwwwwwwwww.lua

--- drawwwwwwwwwwwwwwwwwwwwwww.lua
+++ drawwwwwwwwwwwwwwwwwwwwwww.lua
@@ -89,6 +89,7 @@ local increment = 0.5
 local previewPart = Instance.new("Part")
 previewPart.Anchored = true
 previewPart.CanCollide = false
+previewPart.CanQuery = false
 previewPart.Size = Vector3.new(1, 1, 1)
 previewPart.Transparency = 0.5
 previewPart.Color = Color3.new(1, 1, 1)  -- White color for preview
