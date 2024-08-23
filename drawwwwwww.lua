ad8b6f21a79cabd4fc4af37ea4cbf5f2f578eff7
skidded <131218588+dukapanzer@users.noreply.github.com>
2024-08-23 11:30:50 +0300

    Update and rename drawwwwww.lua to drawwwwwww.lua

---
+++ drawwwwwww.lua
@@ -0,0 +1,163 @@
+local plr = owner
+local char = owner.Character
+
+local humanoid = char:WaitForChild("Humanoid")
+
+remotes = {}
+parts = {}
+
+local remote1 = Instance.new("RemoteEvent")
+remote1.Parent = game:GetService("LocalizationService")local plr = owner
+local char = owner.Character
+
+local humanoid = char:WaitForChild("Humanoid")
+
+remotes = {}
+parts = {}
+
+local remote1 = Instance.new("RemoteEvent")
+remote1.Parent = game:GetService("LocalizationService")
+remote1.Name = "CreatePart"
+table.insert(remotes, remote1)
+
+local remote2 = Instance.new("RemoteEvent")
+remote2.Parent = game:GetService("LocalizationService")
+remote2.Name = "ClearParts"
+table.insert(remotes, remote2)
+
+local remote3 = Instance.new("RemoteEvent")
+remote3.Parent = game:GetService("LocalizationService")
+remote3.Name = "setPartTransparency"
+table.insert(remotes, remote3)
+
+local model = Instance.new("Model")
+model.Parent = workspace
+model.Name = "Parts"
+
+remote2.OnServerEvent:connect(function(CFrame)
+    local part = Instance.new("Part")
+    table.insert(parts, part)
+    part.Parent = model
+    part.Size = "1, 1, 1"
+    part.Transparency = 0.5
+    part.CanCollide = false
+    part.CanQuery = false
+    part.CFrame = CFrame
+end)
+
+remote2.OnServerEvent:connect(function()
+    if parts then
+        parts:Destroy()
+    else
+        return
+    end
+end)
+
+remote3.OnServerEvent:connect(function(CFrame)
+    if parts then
+        parts.Transparency = 1
+    else
+        return
+    end      
+end)
+
+humanoid.Died:connect(function()
+    if remotes then
+        remotes:Destroy()
+    end
+end)
+
+NLS([[
+local plr = owner
+local mouse = plr:GetMouse()
+local createPart = game:GetService("LocalizationService"):WaitForChild("CreatePart")
+local clearParts = game:GetService("LocalizationService"):WaitForChild("ClearParts")
+local setTransparency = game:GetService("LocalizationService"):WaitForChild("setPartTransparency")
+
+mouse.Button1Down:connect(function()
+    createPart:FireServer(mouse.Hit.CFrame)
+end)
+
+mouse.KeyDown:connect(function(k)
+    k = k:lower()
+    
+    if k == "R" then
+        clearParts:FireServer()
+    end
+
+    if k == "F" then
+        setTransparency:FireServer()
+    end
+end)
+]])
+remote1.Name = "CreatePart"
+table.insert(remotes, remote1)
+
+local remote2 = Instance.new("RemoteEvent")
+remote2.Parent = game:GetService("LocalizationService")
+remote2.Name = "ClearParts"
+table.insert(remotes, remote2)
+
+local remote3 = Instance.new("RemoteEvent")
+remote3.Parent = game:GetService("LocalizationService")
+remote3.Name = "setPartTransparency"
+table.insert(remotes, remote3)
+
+local model = Instance.new("Model")
+model.Parent = workspace
+model.Name = "Parts"
+
+remote2.OnServerEvent:connect(function(CFrame)
+    local part = Instance.new("Part")
+    table.insert(parts, part)
+    part.Parent = model
+    part.Size = "1, 1, 1"
+    part.Transparency = 0.5
+    part.CanCollide = false
+    part.CanQuery = false
+    part.CFrame = CFrame
+end)
+
+remote2.OnServerEvent:connect(function()
+    if parts then
+        parts:Destroy()
+    else
+        return
+    end
+end)
+
+remote3.OnServerEvent:connect(function(CFrame)
+    if parts then
+        parts.Transparency = 1
+    else
+        return
+    end      
+end)
+
+humanoid.Died:connect(function()
+    remotes:Destroy()
+end)
+
+NLS([[
+local plr = owner
+local mouse = plr:GetMouse()
+local createPart = game:GetService("ReplicatedStorage"):WaitForChild("CreatePart")
+local clearParts = game:GetService("ReplicatedStorage"):WaitForChild("ClearParts")
+local setTransparency = game:GetService("ReplicatedStorage"):WaitForChild("setPartTransparency")
+
+mouse.Button1Down:connect(function()
+    createPart:FireServer(mouse.Hit.CFrame)
+end)
+
+mouse.KeyDown:connect(function(k)
+    k = k:lower()
+    
+    if k == "R" then
+        clearParts:FireServer()
+    end
+
+    if k == "F" then
+        setTransparency:FireServer()
+    end
+end)
+]])
