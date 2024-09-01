a0981a7ba38730b77c6964f881404ec520deb96c
skidded <131218588+dukapanzer@users.noreply.github.com>
2024-09-01 21:54:33 +0300

    Update and rename dssdkdddfsddfdgdddddddsddddssddjssfddfsfdddsddsssfddsfsdsfsfdsfds.lua to dssddkdddfsddfdgdddddddsddddssddjssfddfsfdddsddsssfddsfsdsfsfdsfds.lua

--- dssddkdddfsddfdgdddddddsddddssddjssfddfsfdddsddsssfddsfsdsfsfdsfds.lua
+++ dssddkdddfsddfdgdddddddsddddssddjssfddfsfdddsddsssfddsfsdsfsfdsfds.lua
@@ -3,12 +3,15 @@ local cursor = Assets:Get("Cursor")
 cursor.Anchored = true
 cursor.Parent = script
 
-
 local plr = owner
 local char = plr.Character
 local remote = Instance.new("RemoteEvent")
 remote.Parent = char
+remote.Name = "remote1"
 cursor.Parent = char
+local remote2 = Instance.new("RemoteEvent")
+remote2.Parent = char
+remote2.Name = "remote2"
 local image_label = cursor:WaitForChild("BillboardGui"):WaitForChild("ImageLabel")
 image_label.Size = UDim2.new(1, 0, 1, 0)
 image_label.Parent.Size = UDim2.new(1, 0, 1, 0)
@@ -40,16 +43,23 @@ remote.OnServerEvent:Connect(function(player, mouse_position, mouse_held, camera
 	end
 end)
 
+remote.OnServerEvent:Connect(function(player, right_held)
+	if right_held then
+		image_label.ImageColor3 = Color3.new(1, 0, 0)
+	else
+		image_label.ImageColor3 = Color3.new(1, 1, 1)
+	end
+end	
 
 NLS([[
 local plr = owner
 local mouse = plr:GetMouse()
 local char = plr.Character
 local cursor = char:WaitForChild("Cursor")
-local remote = char:WaitForChild("RemoteEvent")
+local remote = char:WaitForChild("remote1")
+local remote2 = char:WaitForChild("remote2")
 local hb = game:GetService("RunService").Heartbeat
 local cam = game.Workspace.CurrentCamera
-
 local right_held = false
 local mouse_held = false
 
@@ -79,11 +89,11 @@ end)
 
 mouse.Button2Down:Connect(function()
 	right_held = true
-	remote:FireServer(mouse_position, mouse_held, cam.CFrame, hit_part, right_held)
+	remote2:FireServer(right_held)
 end)
 
 mouse.Button2Up:Connect(function()
 	right_held = false
-	remote:FireServer(mouse_position, mouse_held, cam.CFrame, hit_part, right_held)
+	remote2:FireServer(right_held)
 end)
 ]])
