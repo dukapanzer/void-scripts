5a687ddc28d51ed17cde42bfbf8171ed3ee84fd7
skidded <131218588+dukapanzer@users.noreply.github.com>
2024-09-01 20:23:12 +0300

    Update and rename dssdfsgsssjssfddfsfdddsdsssfddsfsdsfsfdsfds.lua to dssdfsgsdssjssfddfsfdddsdsssfddsfsdsfsfdsfds.lua

--- dssdfsgsdssjssfddfsfdddsdsssfddsfsdsfsfdsfds.lua
+++ dssdfsgsdssjssfddfsfdddsdsssfddsfsdsfsfdsfds.lua
@@ -59,12 +59,14 @@ end)
 mouse.Button1Down:Connect(function()
 	local mouse_position = mouse.Hit.Position
 	hit_part = mouse.Target
+	mouse.TargetFilter = hit_part
 	mouse_held = true
 	remote:FireServer(mouse_position, mouse_held, cam.CFrame, hit_part)
 end)
 
 mouse.Button1Up:Connect(function()
 	local mouse_position = mouse.Hit.Position
+	mouse.TargetFilter = nil
 	mouse_held = false
 	remote:FireServer(mouse_position, mouse_held, cam.CFrame, hit_part)
 end)
