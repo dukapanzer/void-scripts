18f55e2659718a4e194a52c65aeb3c1d2f6b2a52
skidded <131218588+dukapanzer@users.noreply.github.com>
2024-09-01 21:57:24 +0300

    Update and rename dssddkdddfsfdddfdgdddddddsddddssddjssfddfsfdddsddsssfddsdfsdsfsfdsfds.lua to dssddkdddfsfddddfdgdddddddsddddssddjssfddfsfdddsddsssfddsdfsdsfsfdsfds.lua

--- dssddkdddfsfddddfdgdddddddsddddssddjssfddfsfdddsddsssfddsdfsdsfsfdsfds.lua
+++ dssddkdddfsfddddfdgdddddddsddddssddjssfddfsfdddsddsssfddsdfsdsfsfdsfds.lua
@@ -18,6 +18,17 @@ image_label.Parent.Size = UDim2.new(1, 0, 1, 0)
 
 local base_offset = Vector3.new(0.9, -0.9, 0)
 
+remote2.OnServerEvent:Connect(function(player, right_held)
+	print("hi")
+	if right_held then
+		print("red")
+		image_label.ImageColor3 = Color3.new(1, 0, 0)
+	else
+		print("white")
+		image_label.ImageColor3 = Color3.new(1, 1, 1)
+	end
+end)
+
 remote.OnServerEvent:Connect(function(player, mouse_position, mouse_held, camera_cframe, hit_part)
 	local distance = (mouse_position - camera_cframe.Position).Magnitude
 
@@ -37,15 +48,6 @@ remote.OnServerEvent:Connect(function(player, mouse_position, mouse_held, camera
 		else
 			warn("sorry if i make you mad but the part you clicked got detected as nil and i don't know what to move, sorry. you probably accidentaly clicked in the sky or missed the part? no idea..")
 		end
-		image_label.ImageColor3 = Color3.new(1, 0, 0)
-	else
-		image_label.ImageColor3 = Color3.new(1, 1, 1)
-	end
-end)
-
-remote2.OnServerEvent:Connect(function(player, right_held)
-	print("hi")
-	if right_held then
 		image_label.ImageColor3 = Color3.new(1, 0, 0)
 	else
 		image_label.ImageColor3 = Color3.new(1, 1, 1)
