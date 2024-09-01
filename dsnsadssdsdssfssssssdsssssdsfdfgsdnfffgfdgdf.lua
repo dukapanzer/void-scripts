ec66f77e741c92280744f5a2aaea9233b30bcae6
skidded <131218588+dukapanzer@users.noreply.github.com>
2024-09-01 17:23:19 +0300

    Update and rename dsnsadssdsdssfsssssdsssssdsfdfgsdnfffgfdgdf.lua to dsnsadssdsdssfssssssdsssssdsfdfgsdnfffgfdgdf.lua

--- dsnsadssdsdssfssssssdsssssdsfdfgsdnfffgfdgdf.lua
+++ dsnsadssdsdssfssssssdsssssdsfdfgsdnfffgfdgdf.lua
@@ -10,13 +10,19 @@ remote.Parent = char
 cursor.Parent = char
 local image_label = cursor:WaitForChild("BillboardGui"):WaitForChild("ImageLabel")
 
-local base_offset = Vector3.new(-0.1, -0.7, 0)
+local base_offset = Vector3.new(-0.4, -0.3, 0)
 
 remote.OnServerEvent:Connect(function(player, mouse_position, mouse_held, camera_cframe)
     local distance = (mouse_position - camera_cframe.Position).Magnitude
 
-    local adjusted_offset = base_offset * (distance / 50)
-    cursor.CFrame = CFrame.new(mouse_position + adjusted_offset)
+    local adjusted_offset = base_offset * (distance / 50) 
+
+    local camera_right_vector = camera_cframe.RightVector
+    local camera_up_vector = camera_cframe.UpVector
+
+    local final_offset = (camera_right_vector * adjusted_offset.X) + (camera_up_vector * adjusted_offset.Y)
+
+    cursor.CFrame = CFrame.new(mouse_position + final_offset)
 
     if mouse_held then
         image_label.ImageColor3 = Color3.new(1, 0, 0)
