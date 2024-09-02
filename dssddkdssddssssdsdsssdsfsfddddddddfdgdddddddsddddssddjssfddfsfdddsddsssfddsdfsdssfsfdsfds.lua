55bf9d2fab6025475c36fd984a7f24c6aa2f1ed8
skidded <131218588+dukapanzer@users.noreply.github.com>
2024-09-02 18:25:38 +0300

    Update and rename dssddkdssddssddsssdsfsfddddddddfdgdddddddsddddssddjssfddfsfdddsddsssfddsdfsdssfsfdsfds.lua to dssddkdssddssssdsdsssdsfsfddddddddfdgdddddddsddddssddjssfddfsfdddsddsssfddsdfsdssfsfdsfds.lua

--- dssddkdssddssssdsdsssdsfsfddddddddfdgdddddddsddddssddjssfddfsfdddsddsssfddsdfsdssfsfdsfds.lua
+++ dssddkdssddssssdsdsssdsfsfddddddddfdgdddddddsddddssddjssfddfsfdddsddsssfddsdfsdssfsfdsfds.lua
@@ -7,14 +7,25 @@ local plr = owner
 local char = plr.Character
 local remote = Instance.new("RemoteEvent")
 remote.Parent = char
-remote.Name = "remote1"
+remote.Name = "remote"
 cursor.Parent = char
 local remote2 = Instance.new("RemoteEvent")
-remote2.Parent = char
-remote2.Name = "remote2"
 local image_label = cursor:WaitForChild("BillboardGui"):WaitForChild("ImageLabel")
 image_label.Size = UDim2.new(1, 0, 1, 0)
 image_label.Parent.Size = UDim2.new(1, 0, 1, 0)
+
+remote.OnServerEvent:connect(function(player, hit_part, mouse_held)
+	if mouse_held then
+		image_label.Color = Color3.new(1, 0, 0)
+		if hit_part:IsA("BasePart") then
+			hit_part.Anchored = false
+			hit_part:SetNetworkOwner(plr)
+		end
+	else
+		image_label.Color = Color3.new(1, 1, 1)
+		return
+	end
+end)
 
 game:GetService("RunService").Heartbeat:connect(function()
 	cursor:SetNetworkOwner(plr)
@@ -25,12 +36,13 @@ local plr = owner
 local mouse = plr:GetMouse()
 local char = plr.Character
 local cursor = char:WaitForChild("Cursor")
-local remote = char:WaitForChild("remote1")
-local remote2 = char:WaitForChild("remote2")
+local remote = char:WaitForChild("remote")
 local hb = game:GetService("RunService").Heartbeat
 local cam = game.Workspace.CurrentCamera
+local hit_part = nil
 local right_held = false
 local mouse_held = false
+local hit_part = nil
 
 local fakePart = Instance.new("Part")
 fakePart.Size = cursor.Size
@@ -41,15 +53,31 @@ fakePart.Transparency = 1
 local mouse_filter = {}
 table.insert(mouse_filter, cursor)
 table.insert(mouse_filter, fakePart)
-	
-local hit_part = nil
+
+mouse.Button1Down:connect(function()
+	hit_part = mouse.Target
+	table.insert(hit_part)
+	mouse_held = true
+end)
+
+mouse.Button1Up:connect(function()
+	table.remove(mouse_filter, hit_part)
+	hit_part = nil
+	mouse_held = false
+end)
 
 hb:Connect(function()
 	local mouse_position = mouse.Hit.Position
-	fakePart.CFrame = fakePart.CFrame:Lerp(CFrame.new(mouse_position), 0.4)
+	fakePart.CFrame = fakePart.CFrame:Lerp(CFrame.new(mouse_position), 0.05)
 	cursor.CFrame = fakePart.CFrame
 	for _, table in ipairs(mouse_filter) do
     		mouse.TargetFilter = table
+	end
+
+	if mouse_held then
+		remote:FireServer(hit_part, mouse_held)
+	else
+		return
 	end
 end)
 ]])
