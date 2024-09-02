98aa64f14dfadc994182777939eac3fcf5b9fdff
skidded <131218588+dukapanzer@users.noreply.github.com>
2024-09-02 18:01:11 +0300

    Update and rename dssddkdsdddsfsfddddddddfdgdddddddsddddssddjssfddfsfdddsddsssfddsdfsdsfsfdsfds.lua to dssddkdsdddssdsfsfddddddddfdgdddddddsddddssddjssfddfsfdddsddsssfddsdfsdsfsfdsfds.lua

---
+++ dssddkdsdddssdsfsfddddddddfdgdddddddsddddssddjssfddfsfdddsddsssfddsdfsdsfsfdsfds.lua
@@ -0,0 +1,42 @@
+local Assets = LoadAssets(110160236417085)
+local cursor = Assets:Get("Cursor")
+cursor.Anchored = false
+cursor.Parent = script
+
+local plr = owner
+local char = plr.Character
+local remote = Instance.new("RemoteEvent")
+remote.Parent = char
+remote.Name = "remote1"
+cursor.Parent = char
+local remote2 = Instance.new("RemoteEvent")
+remote2.Parent = char
+remote2.Name = "remote2"
+local image_label = cursor:WaitForChild("BillboardGui"):WaitForChild("ImageLabel")
+image_label.Size = UDim2.new(1, 0, 1, 0)
+image_label.Parent.Size = UDim2.new(1, 0, 1, 0)
+
+NLS([[
+local plr = owner
+local mouse = plr:GetMouse()
+local char = plr.Character
+local cursor = char:WaitForChild("Cursor")
+local remote = char:WaitForChild("remote1")
+local remote2 = char:WaitForChild("remote2")
+local hb = game:GetService("RunService").Heartbeat
+local cam = game.Workspace.CurrentCamera
+local right_held = false
+local mouse_held = false
+
+local fakePart = Instance.new("Part")
+fakePart.Size = cursor.Size
+fakePart.Anchored = true
+fakePart.Parent = workspace
+
+local hit_part = nil
+
+hb:Connect(function()
+	local mouse_position = mouse.Hit.Position
+	fakePart.CFrame = fakePart.CFrame:Lerp(CFrame.new(mouse_position), 0.1)
+end)
+]])
