205321614fcd9ce4623c6efda9b0992690d192ec
skidded <131218588+dukapanzer@users.noreply.github.com>
2024-09-02 14:09:40 +0300

    Update and rename dssddkddddfsfdddddddfdgdddddddsddddssddjssfddfsfdddsddsssfddsdfsdsfsfdsfds.lua to dssddkdsdddfsfdddddddfdgdddddddsddddssddjssfddfsfdddsddsssfddsdfsdsfsfdsfds.lua

--- dssddkdsdddfsfdddddddfdgdddddddsddddssddjssfddfsfdddsddsssfddsdfsdsfsfdsfds.lua
+++ dssddkdsdddfsfdddddddfdgdddddddsddddssddjssfddfsfdddsddsssfddsdfsdsfsfdsfds.lua
@@ -50,7 +50,7 @@ remote.OnServerEvent:Connect(function(player, mouse_position, mouse_held, camera
 	else
 		if not right_held then
 			image_label.ImageColor3 = Color3.new(1, 1, 1)
-		end)
+		end
 	end
 end)
 
